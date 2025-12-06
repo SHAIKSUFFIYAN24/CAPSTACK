c0bc6a1ee8061c68b69824b955c423228a41d64b
Shaik.Abdul Sammed <samm41236@gmail.com>
2025-12-06 16:39:33 +0530

    add

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/styles/theme";
+import Head from "next/head";
 
 import Navigation from "../components/Navigation";
 
@@ -27,6 +28,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
+        <Head>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Crect width='24' height='24' fill='%23007AF7'/%3E%3Ctext x='12' y='16' font-size='12' text-anchor='middle' fill='white'%3ECS%3C/text%3E%3C/svg%3E"
+          />
+        </Head>
         <CssBaseline />
         {/* Global Navigation Bar */}
         {showNavbar && <Navigation />}
